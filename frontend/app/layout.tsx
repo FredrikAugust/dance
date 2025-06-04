cbf1c72dfac68f333aa65ba8fd51128c4311fae8
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2025-06-04 18:22:12 +0200

    add analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,6 @@
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,6 +32,7 @@ export default function RootLayout({
 			>
 				{children}
 				<SpeedInsights />
+				<Analytics />
 			</body>
 		</html>
 	);
